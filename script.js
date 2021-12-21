4e796c713ec15c17bda36c273d9ef0e2f6d1cc8d
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2021-12-22 01:41:30 +0200

    Fix the style of the navbar

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const closeItem = document.querySelector('.close');
 
 const home = document.createElement('li');
 const listItem = document.createElement('a');
-listItem.innerText = ('Home');
-listItem.href = ('index.html');
-home.appendChild(listItem);
-navUl.insertBefore(listItem, navUl.childNodes[0]);
-navUl.appendChild(home);
+
 
 const toggleMenu = () => {
   navigation.classList.toggle('menu');
+  listItem.innerText = ('Home');
+listItem.href = ('index.html');
+listItem.classList.add('homeA')
+listItem.classList.toggle('homeLink')
+home.appendChild(listItem);
+navUl.insertBefore(listItem, navUl.childNodes[0]);
+navUl.appendChild(home);
 };
 
 hamburger.addEventListener('click', toggleMenu);
