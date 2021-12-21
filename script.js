77540e8e0d79fda6fe9a47710617bf0aec3da4e7
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2021-12-22 00:44:57 +0200

    Add mobile menu

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.nav-mobile');
+const navigation = document.querySelector('.nav-desktop');
+const navUl = document.querySelector('.nav-desktop > ul');
+const toolbar = document.querySelector('.toolbar');
+const closeItem = document.querySelector('.close');
+
+const home = document.createElement('li');
+const listItem = document.createElement('a');
+listItem.innerText = ('Home');
+listItem.href = ('index.html');
+home.appendChild(listItem);
+navUl.insertBefore(listItem, navUl.childNodes[0]);
+navUl.appendChild(home);
+
+const toggleMenu = () => {
+  navigation.classList.toggle('menu');
+};
+
+hamburger.addEventListener('click', toggleMenu);
+closeItem.addEventListener('click', toggleMenu);
+
